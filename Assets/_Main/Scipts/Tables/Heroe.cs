a36ac36944c4e29b3022a6dc40075045ad138d0c
Juan_Pablor_SR <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2026-05-27 14:42:11 -0600

    connect to data base

---
+++ Assets/_Main/Scipts/Tables/Heroe.cs
@@ -0,0 +1,13 @@
+[System.Serializable]
+public class Heroe
+{
+    public int ID_Heroe;
+    public string Nombre;
+    public string Descripcion;
+    public string URL_Heroe;
+    public float Vida;
+    public float Escudo;
+    public float Armadura;
+    public int Edad;
+    public float Altura;
+}
