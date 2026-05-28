648b2f3d25e066babd270544b00e16414ab194f0
Juan_Pablor_SR <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2026-05-27 18:19:27 -0600

    create tables in class

---
+++ Assets/_Main/Scipts/Tables/Habilidad.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class Habilidad
+{
+    public int ID_Habilidad;
+    public string Nombre;
+    public string URL_Habilidad;
+    public string Descripcion;
+    public float Cooldown;
+    public float Damage;
+    public float Cura;
+}
---
+++ Assets/_Main/Scipts/Tables/Habilidades.cs
@@ -0,0 +1,10 @@
+[System.Serializable]
+public class Habilidades
+{
+    public int ID_Habilidades;
+    public int ID_Heroe;
+    public int ID_Habilidad_1;
+    public int ID_Habilidad_2;
+    public int ID_Habilidad_3;
+    public int ID_Habilidad_4;
+}
